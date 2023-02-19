3652c16fbb5f0701a012598ca3c61d7e8984e768
adriansmagons <124814824+adriansmagons@users.noreply.github.com>
2023-02-19 19:59:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,21 +11,46 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
+    
     for i, next in enumerate(text):
+        # enumerate atdod gan kartas nr gan simbolu
         if next in "([{":
-            # Process opening bracket, write your code here
-            pass
-
+            
+            opening_brackets_stack.append(Bracket(next,i+1))
+            
+        
         if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+                return i+1
+            opening_brackets_stack.pop()   
+    if(not opening_brackets_stack):
+        return "Success"
+    else:
+        return opening_brackets_stack[-1].position
+    
+    
 
 
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    print("Input method:")
+    tekstaievade = input()
+    if tekstaievade.__contains__("I"):
+        print("Input string:")
+        text = input()
+        mismatch = find_mismatch(text)
+        print(mismatch)
+    elif tekstaievade.__contains__("F"):
+        print("Input path to file:")
+        failapath = input()
+        with open(failapath) as f:
+            saturs = f.read()
+        mismatch = find_mismatch(saturs)
+        print(mismatch)
 
+
+    
+    
+   
 
 if __name__ == "__main__":
     main()
